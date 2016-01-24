d693d54da0d34aec74f3540701781aab426b7aff
chileap <chileapchhin@gmail.com>
2016-01-24 19:40:58 +0700

    Login and Sign up with facebook and devise

--- Tourest/config/routes.rb
+++ Tourest/config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
 
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' , registrations: 'registrations' }
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   root 'tourest#index'
+
+  as :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'       
+    put "users/:id" => "users#update"     
+  end
 
   get 'index' =>  'tourest#index'
   get 'about' =>  'tourest#about'
   get 'contact' =>  'tourest#contact'
-  get 'login' =>  'tourest#login'
-  get 'register' =>  'tourest#register'
+
+  get 'users/update'
+
+  get 'users/finish_signup'
 
   resources "tourest"
+
+  resources "users"
   
 end
+
